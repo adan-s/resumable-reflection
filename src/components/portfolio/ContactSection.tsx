e4d478c315af4eed10ed16f1d729350789ada7ac
adansalman <adan.salman@codedistrict.com>
2025-09-20 21:27:05 +0500

    some changes

--- src/components/portfolio/ContactSection.tsx
+++ src/components/portfolio/ContactSection.tsx
@@ -24,7 +24,7 @@ export function ContactSection() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div className="grid grid-cols-1">
           {/* Contact Information */}
           <div className="space-y-8 animate-slide-up">
             <div>
