e20195dd2d36424a65c4b6b30570337b4b532eb4
adansalman <adan.salman@codedistrict.com>
2025-09-23 12:35:52 +0500

    some changes

--- src/components/portfolio/ProjectsSection.tsx
+++ src/components/portfolio/ProjectsSection.tsx
@@ -95,12 +95,12 @@ export function ProjectsSection() {
           ))}
         </div>
 
-        <div className="text-center mt-12">
+        {/* <div className="text-center mt-12">
           <Button variant="outline" size="lg">
             <Github className="w-5 h-5 mr-2" />
             View All Projects on GitHub
           </Button>
-        </div>
+        </div> */}
       </div>
     </section>
   );
